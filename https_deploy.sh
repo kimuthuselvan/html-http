759e457a0a89ed1ca102338f286689b5cca3979f
kimuthuselvan <32721869+kimuthuselvan@users.noreply.github.com>
2017-10-12 17:34:52 -0400

    Update https_deploy.sh

--- https_deploy.sh
+++ https_deploy.sh
@@ -2,22 +2,23 @@
 
 _exit_status ()
 {
-if [ $? -eq 0 ]
-then
-echo "Sucess"
-else
-echo "Failed"
-exit 1
-fi
+	if [ $? -eq 0 ]
+	then
+		echo "Sucess"
+	else
+		echo "Failed"
+		exit 1
+	fi
 }
 
-HTTP_HTML=/var/www/html
+DOCUMENTROOT=/var/www/html/ec2-34-215-139-99.us-west-2.compute.amazonaws.com/public_html
 WORKSPACE=/opt/jenkins/workspace/html-http
+
 echo -e "Starting deployment at https server ... \c"
-cp -f ${WORKSPACE}/index.html ${HTTP_HTML}/
+cp -f ${WORKSPACE}/index.html ${DOCUMENTROOT}/
 _exit_status
 
 echo -e "Changing permission ... \c"
-cd ${HTTP_HTML}
+cd ${DOCUMENTROOT}
 chmod 664 index.html
 _exit_status
