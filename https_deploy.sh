7df4c0d2bbb4a3dbe94f84af7fcf2e2425f10314
kimuthuselvan <32721869+kimuthuselvan@users.noreply.github.com>
2017-10-12 11:16:44 -0400

    Update https_deploy.sh

--- https_deploy.sh
+++ https_deploy.sh
@@ -3,7 +3,7 @@
 HTTPS_PUBLIC_HTML=/var/www/ec2-34-215-139-99.us-west-2.compute.amazonaws.com/public_html
 WORKSPACE=/opt/jenkins/workspace/html-http
 echo -e "Starting deployment at https server ... \c"
-cp -f ${WORKSPACE}/*.html ${HTTPS_PUBLIC_HTML}/*.html
+cp -f ${WORKSPACE}/*.html ${HTTPS_PUBLIC_HTML}/
 if [ $? -eq 0 ]
 then
 echo "Sucess"
