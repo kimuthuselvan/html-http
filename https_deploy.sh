353ed58b64532997246f5e4dbb1e1b13884dc94e
kimuthuselvan <32721869+kimuthuselvan@users.noreply.github.com>
2017-10-12 12:44:29 -0400

    Update https_deploy.sh

--- https_deploy.sh
+++ https_deploy.sh
@@ -3,10 +3,12 @@
 HTTP_HTML=/var/www/html
 WORKSPACE=/opt/jenkins/workspace/html-http
 echo -e "Starting deployment at https server ... \c"
-cp -f ${WORKSPACE}/*.html ${HTTP_HTML}/
+cp -f ${WORKSPACE}/index.html ${HTTP_HTML}/
 if [ $? -eq 0 ]
 then
 echo "Sucess"
+exit 0
 else
 echo "Failed"
+exit 1
 fi
